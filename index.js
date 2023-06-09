9645cda362504e52d853365fac2c918843a015b9
Brooke Morrison <bwookieeeee@gmail.com>
2023-06-09 21:29:12 +0000

    add get user rte

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require('dotenv').config();
 const express = require("express");
 const uuid = require("uuid");
+const Client = require("pg").Client;
+const client = new Client( {
+  connectionString: process.env.DB_CONNECTIONSTRING
+})
 
 const app = express()
 const port = process.env.EX_PORT;
@@ -13,7 +17,31 @@ app.get('/', (req, res) => {
   }))
 })
 
-app.post('/users/create', (req, res) => {
+
+app.get('/user', async (req, res) => {
+  // TODO: authorization
+  try {
+
+    client.connect();
+    const ret = await client.query("SELECT * FROM users WHERE email=$1::text LIMIT 1", [req.body.target]);
+    client.end();
+    
+    const t = ret.rows[0];
+    res.status(200).json(JSON.stringify({
+      id: t.id,
+      pwHash: t.pw_hash,
+      email: t.email,
+      permissions: t.permissions,
+      firstName: t.first_name,
+    lastName: t.last_name
+  }))
+    } catch (e) {
+      console.error(e);
+      res.sendStatus(500);
+    }
+})
+
+app.post('/user/create', (req, res) => {
   const body = JSON.parse(req.body);
   
 })
