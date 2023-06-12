fffc18a9d6d39104ccb0ba5e75c0f6cc67e0e3bc
brooke morrison <bwookieeeee@gmail.com>
2023-06-11 18:38:00 -0700

    remove weird string encapsulation on json objects

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const uuid = require("uuid");
 const Client = require("pg").Client;
-const client = new Client( {
+const client = new Client({
   connectionString: process.env.DATABASE_URL
 })
 
@@ -12,40 +12,41 @@ const port = process.env.EX_PORT;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.json(JSON.stringify({
+  res.json({
     version: require("./package.json").version
-  }))
+  })
 })
 
 
 app.get('/user', async (req, res) => {
+  console.log("got user request");
   // TODO: authorization
   try {
 
     client.connect();
     const ret = await client.query("SELECT * FROM users WHERE email=$1::text LIMIT 1", [req.body.target]);
     client.end();
-    
+
     const t = ret.rows[0];
-    res.status(200).json(JSON.stringify({
+    res.status(200).json({
       id: t.id,
       pwHash: t.pw_hash,
       email: t.email,
       permissions: t.permissions,
       firstName: t.first_name,
-    lastName: t.last_name
-  }))
-    } catch (e) {
-      console.error(e);
-      res.sendStatus(500);
-    }
+      lastName: t.last_name
+    })
+  } catch (e) {
+    console.error(e);
+    res.sendStatus(500);
+  }
 })
 
 app.post('/user/create', (req, res) => {
   const body = JSON.parse(req.body);
-  
+
 })
 
 app.listen(port, () => {
-  console.log("Listening on port: ", port);
+  console.log(`Listening on ${process.env.CONSOLE_URL}:${port}`);
 })
