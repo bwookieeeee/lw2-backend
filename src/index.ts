03af53fd959bf58d8cb001ab8a4f8b26bed80b05
Brooke Morrison <bwookieeeee@gmail.com>
2023-06-14 21:19:08 +0000

    I hate that this is how I have to debug

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ app.post("/user", async (req, res) => {
   const lastName: string | null = req.body.lastName || null;
 
   try {
-    const q: QueryResult = await client.query("INSERT INTO users (id, email, pw_hash, permissions, firstName, lastName) VALUES ($1,$2,$3,$4,$5,$6)", [
+    const q: QueryResult = await client.query("INSERT INTO users (id, email, pw_hash, permissions, first_name, last_name) VALUES ($1,$2,$3,$4,$5,$6)", [
       id, email, pwHash, permissions, firstName, lastName
     ]);
     res.status(201).json({
